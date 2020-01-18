b9bb4619359c934b52cdaa90df981e7ed96e4e98
Anton Marin <antonaryo85@gmail.com>
2020-01-18 16:35:41 +0300

    rollback deleting network

--- deploy/main.tf
+++ deploy/main.tf
@@ -7,6 +7,10 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "f1-micro"
@@ -19,6 +23,7 @@ resource "google_compute_instance" "vm_instance" {
   }
 
   network_interface {
+    network = google_compute_network.vpc_network.name
     access_config {
     }
   }
