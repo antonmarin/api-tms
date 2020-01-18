c2814de7759651f8bb4b191a2bff8daf0aa2778a
Anton Marin <antonaryo85@gmail.com>
2020-01-18 16:07:27 +0300

    #1 testing changing infra

--- deploy/main.tf
+++ deploy/main.tf
@@ -14,10 +14,11 @@ resource "google_compute_network" "vpc_network" {
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "f1-micro"
+  tags         = ["http-server", "https-server"]
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "cos-cloud/cos-stable"
     }
   }
 
