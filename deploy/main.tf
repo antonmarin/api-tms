6236a94f57685a06b1729948ad5b1559f213ea7b
Anton Marin <antonaryo85@gmail.com>
2020-01-18 17:35:59 +0300

    #1 custom network is not required

--- deploy/main.tf
+++ deploy/main.tf
@@ -7,10 +7,6 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
-}
-
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "f1-micro"
@@ -23,6 +19,7 @@ resource "google_compute_instance" "vm_instance" {
   }
 
   network_interface {
+    network = "default"
     access_config {
     }
   }
