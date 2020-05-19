b5f4d99408285a0d15df42d1be7fc3355796a1bf
Anton Marin <antonaryo85@gmail.com>
2020-05-19 23:18:01 +0300

    move to platform

--- deploy/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "remote" {
-    organization = "antonmarin"
-
-    workspaces {
-      name = "api-tms-prod"
-    }
-  }
-}
--- deploy/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "google_service_account" {
-  type = string
-  description = "JSON string of google service account key file used to access GCP"
-}
